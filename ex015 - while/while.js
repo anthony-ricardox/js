566ce03721b033be4e9d8842ca49331373bcf46e
anthony-ricardox <anthonyricardoact9@gmail.com>
2023-10-18 21:07:45 -0300

    exercicio novo

---
+++ ex015 - while/while.js
@@ -0,0 +1,19 @@
+
+
+/*var a = 1
+while(a <= 100){   
+    console.log(`passo ${a}`)
+    a++
+}
+esse metodo de repeticao testa primeiro, depois executa o bloco */
+
+
+var time = 1
+ do{ // do = faça - em português
+    console.log(`oii ${time}`)
+    time++
+    
+ }while(time <= 5)
+ // esse metodo com o do , primeiro executa o bloco  e depois testa
+ 
+
---
+++ ex016 - for/for.js
@@ -0,0 +1,10 @@
+/*var a = 1
+
+while(a <= 5){  
+    console.log(`Passo ${a}`)
+    a++
+}*/
+
+for( a = 1 ; a <= 5 ; a++){  
+    console.log(`Passo ${a}`)
+}
