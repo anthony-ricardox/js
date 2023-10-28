09975ffdcc7bb82b701a26a1ddfab3dc1fd717a4
anthony-ricardox <anthonyricardoact9@gmail.com>
2023-10-28 19:56:02 -0300

    ex funcao return

---
+++ ex20 - funcao return/imparpar.js
@@ -0,0 +1,18 @@
+function parimp(n){ 
+    if(n % 2 == 0){   
+        return 'PAR'
+    }else{  
+        return 'IMPAR'
+    }
+}
+
+let res = parimp(9883)
+console.log(res)
+
+
+    
+    
+
+    
+    
+    
